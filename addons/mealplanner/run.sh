b3e74bd55c0cb44e0baa0a17c2041d640963de45
AdminApple <harryfabb@gmail.com>
2026-03-04 09:36:13 +0000

    fixed .sh error

--- addons/mealplanner/run.sh
+++ addons/mealplanner/run.sh
@@ -6,4 +6,4 @@ bashio::log.info "Starting Meal Planner add-on..."
 export MEALPLANNER_DB_PATH="/data/mealplanner.db"
 export MEALPLANNER_INGRESS_PATH="${INGRESS_PATH:-/}"
 
-python -m uvicorn server:app --host 0.0.0.0 --port 8099
+exec python3 -m uvicorn server:app --host 0.0.0.0 --port 8099
