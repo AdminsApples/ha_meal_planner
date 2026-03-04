9e30b0f78aaf43fd7b6053490f87dd8b8f290a3c
AdminApple <harryfabb@gmail.com>
2026-03-04 09:45:08 +0000

    0.4.02

--- addons/mealplanner/run.sh
+++ addons/mealplanner/run.sh
@@ -6,4 +6,5 @@ bashio::log.info "Starting Meal Planner add-on..."
 export MEALPLANNER_DB_PATH="/data/mealplanner.db"
 export MEALPLANNER_INGRESS_PATH="${INGRESS_PATH:-/}"
 
-exec python3 -m uvicorn server:app --host 0.0.0.0 --port 8099
+# Use venv python (PATH is set in Dockerfile, but keeping it explicit is fine)
+exec /opt/venv/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8099
